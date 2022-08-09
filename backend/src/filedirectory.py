954667ab9ec2057df1273cd0fc4a9a2790bf616d
yshimizu20 <yukohshimizu@icloud.com>
2022-08-10 08:58:18 +0900

    text extraction code added

--- backend/src/filedirectory.py
+++ backend/src/filedirectory.py
@@ -3,20 +3,23 @@ import os
 from datetime import datetime
 from time import strptime
 import csv
+from PyPDF2 import PdfReader
+import docx
 
 
 class FileDirectory:
-    def __init__(self, dirpath="samples"):
+    def __init__(self, dirpath="samples", extracted_dir="samples/extracted/"):
         self.dirpath = dirpath
-    
+        self.extracted_dir = extracted_dir
+
     def scrape_title(self):
         """
         scrapes all files in the samples/ directory
         stores all file info in samples/filepaths.csv
         """
-        
+
         q = [self.dirpath]
-        titles = []
+        self.titles = []
 
         while q:
             d = q.pop()
@@ -33,7 +36,7 @@ class FileDirectory:
                 else:
                     doctype = parsed_lst[-1]
 
-                titles.append({
+                self.titles.append({
                     "filepath": filepath,
                     "date_mod": datetime.strftime(date_mod, "%Y-%m-%d"),
                     "date_cre": datetime.strftime(date_cre, "%Y-%m-%d"),
@@ -41,19 +44,49 @@ class FileDirectory:
                     "scraped_extracted": "",
                     "scraped_plain": ""
                 })
-            print(titles)
+            print(self.titles)
 
             field_names = ["filepath", "date_mod",
-                        "date_cre", "doctype", "scraped_extracted", "scraped_plain"]
+                           "date_cre", "doctype", "scraped_extracted", "scraped_plain"]
             with open("samples/filepaths.csv", "w+") as f:
                 writer = csv.DictWriter(f, fieldnames=field_names)
                 writer.writeheader()
-                writer.writerows(titles)
+                writer.writerows(self.titles)
 
-        return titles
+        return self.titles
 
     def scrape_files(self):
-        pass
+        for file in self.titles:
+            parsed = file["filepath"].split('/')[-1].split('.')
+            if len(parsed) <= 1:
+                continue
+            extension = parsed[-1]
 
-    def update_filepaths(event):
-        pass
+            if extension == "txt":
+                with open(self.dirpath + file["filepath"], 'r') as f:
+                    contents = ''.join(f.readlines()).lower()
+                with open(self.extracted_dir + file["filepath"] + '.txt', 'w') as f:
+                    f.write(contents)
+            
+            elif extension == "pdf":
+                reader = PdfReader(self.dirpath + file["filepath"])
+                contents = ''.join(reader.pages[0].extractText())
+                with open(self.extracted_dir + file["filepath"] + '.txt', 'w') as f:
+                    f.write(contents)
+            
+            elif extension == "docx":
+                document = docx.Document(self.dirpath + file["filepath"])
+                contents = ''.join(document.paragraphs)
+                # think about how to process tables
+                tables = document.tables
+
+            elif extension == "csv":
+                # think about how to process csv tables
+                pass
+            
+            elif extension in ["png", "jpeg", "jpg", "tiff"]:
+                # think about how to process images
+                pass
+
+            else:
+                pass
